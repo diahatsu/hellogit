7025ad3c2bc982cdd8a5f40fe1f9b225b8a25b39
diahatsu <diahatsu@users.noreply.github.com>
2018-01-31 11:02:12 -0400

    Added main file

---
+++ breakfast.cpp
@@ -0,0 +1,28 @@
+//samuel hinds assignment 0
+#include<iostream>
+#include<vector>
+#include "toast.h" //class file
+using namespace std;
+
+int main()
+{
+    vector<toast> breakfast; //vector using toast class
+    int temp_int; //temp variable to hold input
+
+    cout << "how do you want your 4 slices of toast, enter 4 ";
+    cout << "numbers: 0=untoasted to 100=burnt" << endl;
+
+    for(int i = 0 ; i < 4 ; i++) //loop to get 4 inputs
+    {
+        cin >> temp_int;
+        breakfast.push_back (temp_int);
+    }
+
+    for(int i = 1 ; i < 5 ; i++) //loop to print 4 vector elements
+    {
+        cout << "Toast #" << i << " is toasted at level ";
+        cout << breakfast[i].getToastLevel() << "//100" << endl;
+    }
+
+    return 0;
+}
