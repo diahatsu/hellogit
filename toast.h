b7acfbff752a48a5dad6b8a8471c3ed2bf627bf9
diahatsu <diahatsu@users.noreply.github.com>
2018-01-31 10:48:26 -0400

    Added header file

---
+++ toast.h
@@ -0,0 +1,41 @@
+class toast
+{
+    private:
+        int toast_level:
+        int toast_id;
+
+    public:
+        toast() //overloaded constructor 1/3
+        {
+            toast_level = 50;
+            toast_id = 0;
+        }
+        toast(int level) //overloade constructor 2/3
+        {
+            toast_level = level;
+            toast_id = 0;
+        }
+        toast(int level, int id) //overloaded constructor 3/3
+        {
+            toast_level = level;
+            toast_id = id;
+        }
+
+        void setToastLevel(int level) //set fuction (level)
+        {
+            toast_level = level;
+        }
+        int getToastLevel() //get function (level)
+        {
+            return toast_level;
+        }
+
+        void setToastID(int id) //set function (id)
+        {
+            toast_id = id;
+        }
+        int getToastLevel() //get function (id)
+        {
+            return toast_id;
+        }
+};
